ad865513953a6daa318bcd59d6f6e7ecc7e7e2b1
victoria795 <vika.shintyabina.02@mail.ru>
2023-04-22 17:28:54 +0300

    добавила environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    api: 'http://90.156.209.122:5000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    api: 'http://90.156.209.122:5000'
+};
