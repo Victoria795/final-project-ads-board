dd94896869ee6fc1460052d7d69a8b812f7a33e0
victoria795 <vika.shintyabina.02@mail.ru>
2023-05-10 01:10:57 +0300

    обновила количество опций для фильтрации

--- src/app/modules/main-page/pages/search/search.component.ts
+++ src/app/modules/main-page/pages/search/search.component.ts
@@ -33,10 +33,28 @@ export class SearchComponent implements OnInit {
     return advert.price >= minPrice
         && advert.price <= maxPrice
   })
+  
   }
 
-  public filterByOptions(){
-  this.ads?.reverse();
+  public filterByOptions(value:any){
+  switch(value.name){
+    case 'новизне':
+    this.ads?.sort((min, max) => new Date(max.createdAt).getTime() - new Date(min.createdAt).getTime()
+    );
+    break;
+    case 'сначала старые':
+    this.ads?.sort((min, max) => new Date(min.createdAt).getTime() - new Date(max.createdAt).getTime()
+    );
+    break;
+    case 'дешевле':
+    this.ads?.sort((min, max) => min.price - max.price);
+    break;
+    case 'дороже':
+    this.ads?.sort((min, max) => max.price - min.price);
+    break;
+  }
+  console.log(value);
+  
   }
 
   private transformCategories(categories: ICategory[]): TreeNode[] {
@@ -87,6 +105,8 @@ export class SearchComponent implements OnInit {
     this.sortOptions = [
       { name: 'новизне' },
       { name: 'сначала старые' },
+      { name: 'дешевле' },
+      { name: 'дороже' }
   ];
   }
   }
