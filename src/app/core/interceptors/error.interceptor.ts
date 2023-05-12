87da6f7e8facde733fcf7ad6fd5a8552a527e5e4
victoria795 <vika.shintyabina.02@mail.ru>
2023-05-12 14:53:39 +0300

    поправочка

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -20,10 +20,14 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       retry(1),
       catchError((err) => {
+        let errorMessage = '';
         if(err.status === 404){
           this._router.navigateByUrl('**');
         } else if (err.status === 400 || err.status === 500){
           this._messageService.add({severity: 'error', summary: 'Ошибка! Попробуйте еще раз'});
+        } else {
+          errorMessage = `Error Status: ${err.status} \n Message: ${err.message}`;
+          this._messageService.add({severity: 'error', summary: errorMessage , detail: ''});
         }
         return throwError(err);
         })
